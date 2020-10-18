625f73fe3c7201e6882be0df7483ff63e53ab558
hereticsanarchy <matt.clark.jobs@gmail.com>
2020-10-17 21:11:53 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 #Please read selenium documentation. You will need webdriver for your version of google chrome for this code to work as-is.
 
@@ -8,6 +9,7 @@ driver = webdriver.Chrome()  # Optional argument, if not specified will search p
 driver.get('http://www.instagram.com/');
 time.sleep(2)
 
+#your username and password here
 username = ''
 pw = ''
 
@@ -25,13 +27,13 @@ def Login():
 
     time.sleep(3)
 
-def Notifications():
+def Notifications(): #Turns off notifications. If pop-up doesn't occur for you, comment out. 
     notifications= driver.find_element_by_tag_name('button.aOOlW.HoLwm')
     notifications.click()
 
 def Search():
     search = driver.find_element_by_class_name('XTCLo.x3qfX')
-    search.send_keys('The White Stripes')
+    search.send_keys('') # Your search term goes here
     time.sleep(2)
     search.send_keys(Keys.RETURN)
     search.send_keys(Keys.RETURN)
@@ -44,9 +46,13 @@ def First_post():
 
 
 def Like():
-    like = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button')
-    like.click()
+    def Like():
 
+        try:
+            like = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button')
+            like.click()
+        except NoSuchElementException:
+            pass
 
 def NextPost():
     nextPost = driver.find_element_by_class_name('_65Bje.coreSpriteRightPaginationArrow')
@@ -60,10 +66,10 @@ Search()
 First_post()
 
 i = 0
-while i < 6:
+while i < 100: #Set for how many times you want to run. Would suggest less than 300 to start. 
 
-    time.sleep(5)
+    time.sleep(10)
     Like()
-    time.sleep(3)
+    time.sleep(10)
     NextPost()
     i+=1
